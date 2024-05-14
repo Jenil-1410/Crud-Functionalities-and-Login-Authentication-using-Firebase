029a47d405894ca266078f07dfb139a5b0024d6f
Jenil Patel <jenilp1410@gmail.com>
2024-05-14 18:08:19 +0530

    Second Commit

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"
+import conf from "./conf/conf";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAF9z9RbkT3ezcaMn9ohYugixcjx3wpDcI",
-  authDomain: "crud-2c8ab.firebaseapp.com",
-  projectId: "crud-2c8ab",
-  storageBucket: "crud-2c8ab.appspot.com",
-  messagingSenderId: "635278718499",
-  appId: "1:635278718499:web:422da19571c5c690772396",
-  measurementId: "G-WWEEX4Z1KF"
+  apiKey: conf.firebaseApikey,
+  authDomain: conf.firebaseAuthdomain,
+  projectId: conf.firebaseProjectId,
+  storageBucket: conf.firebaseStoragebucket,
+  messagingSenderId: conf.firebaseMessagingsenderId,
+  appId: conf.firebaseAppId,
+  measurementId: conf.firebaseMeasurementId
 };
 
 const app = initializeApp(firebaseConfig);
